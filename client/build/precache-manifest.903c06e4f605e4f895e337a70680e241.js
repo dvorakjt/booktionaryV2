f201f6e2ff20611cf2f9ceea1295e934660c85e4
Joseph Dvorak <dvorakjt@gmail.com>
2020-07-26 23:31:11 -0400

    updated for heroku

---
+++ client/build/precache-manifest.903c06e4f605e4f895e337a70680e241.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a181234fd0e455f701d95e67cef0bdc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f632401eb52afffe29e",
+    "url": "/static/css/main.57dddb37.chunk.css"
+  },
+  {
+    "revision": "4e6309e673461735ad25",
+    "url": "/static/js/2.6fccd5e2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6fccd5e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f632401eb52afffe29e",
+    "url": "/static/js/main.a65c2628.chunk.js"
+  },
+  {
+    "revision": "283eee3a1ed1f41d2fd6",
+    "url": "/static/js/runtime-main.1247338a.js"
+  },
+  {
+    "revision": "9e88f8ad00b01bb6b76a084078cf3b52",
+    "url": "/static/media/isometric-books-vector.9e88f8ad.jpg"
+  }
+]);
